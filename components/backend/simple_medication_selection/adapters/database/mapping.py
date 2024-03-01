8d1c8f8c3960082c89be4f39e6cee33b988cb870
Ivan Shikhanov <vandersvanlin@gmail.com>
2024-03-01 16:36:01 +0300

    Добавить таблицы и их mapping

--- components/backend/simple_medication_selection/adapters/database/mapping.py
+++ components/backend/simple_medication_selection/adapters/database/mapping.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import registry
+from sqlalchemy.orm import registry, relationship
 
 from simple_medication_selection.application import entities
 
@@ -6,4 +6,40 @@ from . import tables
 
 mapper = registry()
 
+mapper.map_imperatively(entities.Patient, tables.patients)
 mapper.map_imperatively(entities.Symptom, tables.symptoms)
+mapper.map_imperatively(entities.Diagnosis, tables.diagnoses)
+mapper.map_imperatively(entities.ItemCategory, tables.item_categories)
+mapper.map_imperatively(entities.ItemType, tables.item_types)
+mapper.map_imperatively(entities.TreatmentItem, tables.treatment_items)
+mapper.map_imperatively(
+    entities.ItemReview,
+    tables.item_reviews,
+    properties={
+        'item': relationship(entities.TreatmentItem, uselist=False, lazy='joined')
+    }
+)
+mapper.map_imperatively(
+    entities.MedicalBook,
+    tables.medical_books,
+    properties={
+        'symptoms': relationship(
+            entities.Symptom,
+            secondary=tables.medical_books_symptoms,
+            lazy='subquery',
+            cascade='save-update, merge'
+        ),
+        'item_reviews': relationship(
+            entities.ItemReview,
+            secondary=tables.medical_books_item_reviews,
+            lazy='subquery',
+            cascade='save-update, merge'
+        ),
+        'diagnosis': relationship(
+            entities.Diagnosis, uselist=False, lazy='joined'
+        ),
+        'patient': relationship(
+            entities.Patient, uselist=False, lazy='joined', backref='medical_books'
+        ),
+    }
+)
--- components/backend/simple_medication_selection/adapters/database/tables.py
+++ components/backend/simple_medication_selection/adapters/database/tables.py
@@ -1,11 +1,14 @@
 from sqlalchemy import (
+    Boolean,
     Column,
+    DECIMAL,
     Float,
     ForeignKey,
     Integer,
     MetaData,
     String,
     Table,
+    Text,
 )
 
 naming_convention = {
@@ -18,15 +21,92 @@ naming_convention = {
 
 metadata = MetaData(naming_convention=naming_convention)
 
+patients = Table(
+    'patients',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('nickname', String, nullable=False, unique=True),
+    Column('gender', String, nullable=False),
+    Column('age', Integer, nullable=False),
+    Column('skin_type', String, nullable=False),
+    Column('about', Text, nullable=True),
+    Column('phone', String(15), nullable=True),
+    Column('profile_photo_path', String(500)),
+)
+
 symptoms = Table(
     'symptoms',
     metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
     Column('name', String, nullable=False, unique=True),
 )
+
 diagnoses = Table(
     'diagnoses',
     metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
     Column('name', String, nullable=False, unique=True),
+)
+
+item_types = Table(
+    'item_types',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String, nullable=False, unique=True),
+)
+
+item_categories = Table(
+    'item_categories',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String, nullable=False, unique=True),
+)
+
+treatment_items = Table(
+    'treatment_items',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('title', String, nullable=False),
+    Column('price', Float, nullable=True),
+    Column('description', String, nullable=True),
+    Column('type_id', Integer, ForeignKey('item_types.id'), nullable=False),
+    Column('category_id', Integer, ForeignKey('item_categories.id'), nullable=False),
+)
+
+item_reviews = Table(
+    'item_reviews',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('item_id', Integer, ForeignKey('treatment_items.id'), nullable=False),
+    Column('is_helped', Boolean, nullable=False),
+    Column('item_rating', DECIMAL(precision=2, scale=1), nullable=False),
+    Column('item_count', Integer, nullable=False),
+    Column('usage_period', Integer, nullable=True),
+)
+
+medical_books_symptoms = Table(
+    'medical_books_symptoms',
+    metadata,
+    Column('medical_book_id', ForeignKey('medical_books.id'), primary_key=True,
+           nullable=False),
+    Column('symptom_id', ForeignKey('symptoms.id'), primary_key=True, nullable=False)
+)
+
+medical_books_item_reviews = Table(
+    'medical_books_item_reviews',
+    metadata,
+    Column('medical_book_id', ForeignKey('medical_books.id'), primary_key=True,
+           nullable=False),
+    Column('item_review_id', ForeignKey('item_reviews.id'), primary_key=True,
+           nullable=False)
+)
+
+medical_books = Table(
+    'medical_books',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('title_history', String(255), nullable=False),
+    Column('history', Text, nullable=True),
+    Column('patient_id', Integer, ForeignKey('patients.id')),
+    Column('diagnosis_id', Integer, ForeignKey('diagnoses.id')),
 )
